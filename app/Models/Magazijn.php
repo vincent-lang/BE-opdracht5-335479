0ed1757a38db645001cae4861b479393bfeee0ed
vincent van de merwe <335479@student.mboutrecht.nl>
2024-01-09 12:54:35 +0100

    next commit

--- app/Models/Magazijn.php
+++ app/Models/Magazijn.php
@@ -4,18 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Magazijn extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'Product_id',
         'VerpakkingsEenheid',
         'AantalAanwezig',
     ];
 
-    public function product()
+    public function Product(): BelongsTo
     {
-        return $this->hasOne(Product::class);
+        return $this->belongsTo(Product::class);
     }
 }
