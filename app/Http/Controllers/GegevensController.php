e0173ad5378693e749f1fce269544911f78586c1
vincent van de merwe <335479@student.mboutrecht.nl>
2024-01-09 12:53:27 +0100

    next commit

--- app/Http/Controllers/GegevensController.php
+++ app/Http/Controllers/GegevensController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Magazijn;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class GegevensController extends Controller
 {
     public function index()
     {
-        return view('snoep.index');
+        return view('snoep.index', [
+            'magazijns' => Magazijn::with('Product')->orderBy(Product::select('barcode')->whereColumn('products.id', 'magazijns.product_id'))->get()
+        ]);
     }
 }
